a3d93cd3a0c614e70c100a9d8c6ebdc289b2a0dc
base58yyy <base58yyy@gmail.com>
2016-06-24 22:47:01 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ethereum/ethereum-qt
-sudo add-apt-repository ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-qt
+sudo add-apt-repository -y ppa:ethereum/ethereum
 sudo apt-get update
-sudo apt-get install cpp-ethereum
+sudo apt-get install -y cpp-ethereum
 
 ethminer -F http://eth-asia.dwarfpool.com/0x9d5906ad2ee22291aa6f1932e72c796618bc2c21/ss
